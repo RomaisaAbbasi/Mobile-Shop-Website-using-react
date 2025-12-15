70843103f8a610ca9609ccf8951a20fa70177c73
RomaisaAbbasi <156936640+RomaisaAbbasi@users.noreply.github.com>
2025-12-15 22:06:48 +0500

    again add cursor

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -77,7 +77,8 @@ export default function Cart() {
               color: "#fff",
               border: "none",
               borderRadius: "6px",
-              cursor: "pointer",
+               cursor: "pointer",
+             
             }}
           >
             Checkout
